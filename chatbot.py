1efcc2e59341fa336295e88038b0bf9159b8b114
Ian Bravyn. <ianbravynsa@gmail.com>
2024-07-22 14:56:41 +0300

    installed PyYAML , code runs without errors but does not provide the expected output

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from pandasai import Agent
+from config import api_key
 
 # Sample DataFrame
 sales_by_country = pd.DataFrame({
@@ -10,7 +11,7 @@ sales_by_country = pd.DataFrame({
 
 # By default, unless you choose a different LLM, it will use BambooLLM.
 # You can get your free API key signing up at https://pandabi.ai (you can also configure it in your .env file)
-os.environ["PANDASAI_API_KEY"] = ""
+os.environ["PANDASAI_API_KEY"] = api_key
 
 agent = Agent(sales_by_country)
 agent.chat('Which are the top 5 countries by sales?')
